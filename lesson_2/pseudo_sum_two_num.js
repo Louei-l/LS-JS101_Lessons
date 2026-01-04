3b8487444ed80470038611074b2d74b77ddad379
Louei-l <152271180+Louei-l@users.noreply.github.com>
2026-01-04 08:14:54 +0000

    added 1st pseudo code exercise.

---
+++ lesson_2/pseudo_sum_two_num.js
@@ -0,0 +1,33 @@
+/** CASUAL
+ * Sum two numbers function
+ * 
+ * Input: two numbers
+ * Output: the sum of the two numbers
+ * 
+ * Declare a function called 'sum' that accepts two arguments num1 and num2.
+ * Verify that both arguments num1 and num2 are numbers 
+ * declare result which equals to an addition of num1 and num2
+ * log result.
+ * Return result.
+ * 
+ */
+
+/** FORMAL
+ * 
+ * START
+ * 
+ * GET num1
+ * GET num2
+ * 
+ * IF num1 AND num2 are numbers 
+ *  SET result = num1 + num2
+ * 
+ * PRINT result
+ * RETURN result
+ * ELSE 
+ *  PRINT please enter valid numbers
+ *  RETURN null
+ * 
+ * END
+ * 
+ */
