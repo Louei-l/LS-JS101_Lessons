174001048c62fd4183b38d4b91e808ace429479f
Louei-l <152271180+Louei-l@users.noreply.github.com>
2026-01-04 08:47:36 +0000

    pseudo code for array concat string exercise.

---
+++ lesson_2/pseudo_concat_array_stings.js
@@ -0,0 +1,30 @@
+/** CAUSUAL
+ * 
+ * concat array of strings into one string
+ * 
+ * Input: Array with string elements
+ * Output: single string of all elements in the array
+ * 
+ * Declare function that accepts an array.
+ * Verify that input is an array
+ *  - If not stop and return an error.
+ * Use join method and specify space as separator
+ * Return result 
+ * 
+ */
+
+/** FORMAL
+ * 
+ * START
+ * 
+ * GET arr
+ * 
+ * IF arr is array and not empty
+ *  SET result =  concatenate all sting elements of the array into one single string separated by spaces
+ *  PRINT result
+ *  RETURN result
+ * ELSE
+ *  PRINT enter an array
+ *  RETURN null
+ * 
+ */
