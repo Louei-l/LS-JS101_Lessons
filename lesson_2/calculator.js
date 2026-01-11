e6818eb428da39ab70b40b880fe3b62b02e8087e
Louei-l <152271180+Louei-l@users.noreply.github.com>
2026-01-11 11:40:01 +0000

    refactored code.

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,31 +1,59 @@
 /**
  * Ask the user for the first number.
- * Ask the user for the second number. 
+ * Ask the user for the second number.
  * Ask the user for the operation to perform
  * Perform operation on two numbers
  * Log the result
  */
 
-console.log('Welcome to the Calculator!');
-
 const readline = require('readline-sync');
-const firstNUmber = Number(readline.question('Pleaser enter first number: '));
-const secondNUmber = Number(readline.question('Pleaser enter second number: '));
-const operator = readline.question('What operation you wish to perform on the two numbers?\n1) Add 2) Subtract 3) Multiply 4) Divide  ');
-if (operator === '1') {
-  output = firstNUmber + secondNUmber;
+function promt(msg) {
+  console.log(`=> ${msg}`);
 }
 
-if (operator === '2') {
-  output = firstNUmber - secondNUmber;
+function isNotaNumber (num) {
+  return num.trimStart() === '' || Number.isNaN(Number(num));
 }
 
-if (operator === '3') {
-  output = firstNUmber * secondNUmber;
+promt('Welcome to the calculator');
+promt('Pleaser enter first number');
+let firstNUmber = readline.question();
+
+while (isNotaNumber(firstNUmber)) {
+  promt('This does not look like a number, please enter a valid number.');
+  firstNUmber = readline.question();
 }
 
-if (operator === '4') {
-  output = firstNUmber / secondNUmber;
+promt('Pleaser enter second number: ');
+let secondNUmber = readline.question();
+
+while (isNotaNumber(secondNUmber)) {
+  promt('This does not look like a number, please enter a valid number.');
+  secondNUmber = readline.question();
 }
 
-console.log(`the result is ${output}`);
+promt('What operation you wish to perform on the two numbers?\n Enter (1) to Add,  (2) to Subtract,  (3) to Multiply, or (4) to Divide');
+let operator = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operator)) {
+  promt('Pick either 1, 2, 3, or 4 perform an operation.');
+  operator = readline.question();
+}
+
+let output;
+switch (operator) {
+  case '1':
+    output = Number(firstNUmber) + Number(secondNUmber);
+    break;
+  case '2':
+    output = Number(firstNUmber) - Number(secondNUmber);
+    break;
+  case '3':
+    output = Number(firstNUmber) * Number(secondNUmber);
+    break;
+  case '4':
+    output = Number(firstNUmber) / Number(secondNUmber);
+    break;
+}
+
+promt(`The result is ${output}`);
